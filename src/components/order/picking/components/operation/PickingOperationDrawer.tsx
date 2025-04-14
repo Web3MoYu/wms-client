7aa45ee841a2811591685be136c4bf941e10b55c
moyu <3040114965@qq.com>
2025-04-14 13:43:37 +0800

    fix

--- src/components/order/picking/components/operation/PickingOperationDrawer.tsx
+++ src/components/order/picking/components/operation/PickingOperationDrawer.tsx
@@ -266,7 +266,7 @@ const PickingOperationDrawer: React.FC<PickingOperationDrawerProps> = ({
       dataIndex: 'actualQuantity',
       key: 'actualQuantity',
       width: 120,
-      render: (text: number, record: PickingItemVo) => (
+      render: (record: PickingItemVo) => (
         <InputNumber
           min={0}
           max={record.expectedQuantity * 2}
